f2bc3c237a013ff786b31c6dcd7545835de31253
Yash Smart <yash.s4@ahduni.edu.in>
2024-07-22 18:04:02 +0530

    Added live filtering of jobs

---
+++ public/jobs_edit.js
@@ -0,0 +1,68 @@
+function updateList() {
+    let title = document.getElementById('Title').value;
+    let description = document.getElementById('Description').value;
+    let newData = [];
+    for (let i=0;i<job_data.length;i++) {
+        if (job_data[i].title.toLowerCase().includes(title.toLowerCase()) && job_data[i].description.toLowerCase().includes(description.toLowerCase())) {
+            newData.push(job_data[i]);
+        }
+    }
+    let jobs_elements = document.querySelectorAll('.job');
+    for (let i=0;i<jobs_elements.length;i++) {
+        jobs_elements[i].remove();
+    }
+    // console.log(document.querySelector('#job-post'))
+    document.querySelector('#job-post').remove();
+    let job_listings = document.querySelector('.job-listings');
+    for (let i=0;i<newData.length;i++) {
+        let new_job_element = document.createElement('article');
+        new_job_element.classList.add('job');
+        let h3 = document.createElement('h3');
+        h3.textContent = newData[i].title;
+        h3.classList.add('ownFont')
+        let jobDetailsElement = document.createElement('div');
+        jobDetailsElement.classList.add('job-details');
+        let descriptionElement = document.createElement('p');
+        descriptionElement.style.whiteSpace = 'pre-wrap';
+        descriptionElement.innerHTML = '<strong>Description:</strong>'+newData[i].description;
+        let departmentElement = document.createElement('p');
+        departmentElement.innerHTML = '<strong>Department:</strong>'+newData[i].department;
+        let location_type_element = document.createElement('p');
+        location_type_element.innerHTML = '<strong>Location Type:</strong>'+newData[i].location_type;
+        let location_element = document.createElement('p');
+        location_element.innerHTML = '<strong>Location:</strong>'+newData[i].location;
+        let type_element = document.createElement('p');
+        type_element.innerHTML = '<strong>Type:</strong>'+newData[i].type;
+        jobDetailsElement.append(descriptionElement);
+        jobDetailsElement.append(departmentElement);
+        jobDetailsElement.append(location_type_element);
+        jobDetailsElement.append(location_element);
+        jobDetailsElement.append(type_element);
+        let posted_on_element = document.createElement('p');
+        posted_on_element.classList.add('posted-on');
+        posted_on_element.textContent = 'Posted on: '+new Date(newData[i].posted_on).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
+        let last_apply_date_element = document.createElement('p');
+        last_apply_date_element.classList.add('posted-on');
+        last_apply_date_element.textContent = 'Last Apply Date: '+new Date(newData[i].last_apply_date).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
+        let ApplyButton = document.createElement('a');
+        ApplyButton.href = 'applications/'+newData[i].id;
+        ApplyButton.innerHTML = '<button class="apply-button">See Applications</button>';
+        deleteJobButton = document.createElement('a');
+        deleteJobButton.innerHTML = '<button class="apply-button delete-button">Delete Job</button>'
+        new_job_element.append(h3);
+        new_job_element.append(jobDetailsElement);
+        new_job_element.append(posted_on_element);
+        new_job_element.append(last_apply_date_element);
+        new_job_element.append(ApplyButton);
+        new_job_element.append(deleteJobButton)
+        job_listings.append(new_job_element);
+    }
+    let AddButton = document.createElement('a');
+    AddButton.href = '/job-post';
+    AddButton.innerHTML = '<button class="apply-button" id="job-post">New Post</button>';
+    job_listings.append(AddButton);
+}
+let job_data = JSON.parse(document.getElementById('job_data').textContent);
+// console.log(job_data)
+document.getElementById('Title').addEventListener('keyup',updateList);
+document.getElementById('Description').addEventListener('keyup',updateList);
