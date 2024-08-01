2133fb16f97a2b23bb97229d7ba2e20b4505e20e
Yash Smart <yash.s4@ahduni.edu.in>
2024-08-01 17:15:33 +0530

    Added port

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ import nodemailer from "nodemailer";
 env.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const transporter = nodemailer.createTransport({
     service:'gmail',
